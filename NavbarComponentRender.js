0f17dc1ce3d72d293eedc19ae84aaa2badeac014
Daniel700HQ <59784927+Daniel700HQ@users.noreply.github.com>
2025-08-20 12:09:43 -0300

    Add files via upload

---
+++ NavbarComponentRender.js
@@ -0,0 +1,15 @@
+class NavbarComponent extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+
+    // Usamos async/await para manejar la carga asíncrona del archivo HTML
+    async connectedCallback() {
+        const response = await fetch('NavbarComponent.html');
+        const html = await response.text();
+        this.shadowRoot.innerHTML = html;
+    }
+}
+
+window.customElements.define('dark-navbar', NavbarComponent);
